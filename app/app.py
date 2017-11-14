8f7073869cf607b111dff6d486804f111497ccfc
jwright4288 <jwright4288@tamu.edu>
2017-11-13 22:11:57 -0600

    Update app.py
    added factorial

--- app/app.py
+++ app/app.py
@@ -19,3 +19,15 @@ def hash(inp):
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
+
+#Factorial
+num = int(input("Enter a number: "))
+factorial = 1
+if num < 0:
+	print("Error: Factorial does not exist for negative numbers")
+elif num == 0:
+	print("The factorial of 0 is 1")
+else:
+	for i in range(1, num + 1):
+		factorial *= i
+	print("The factorial of", num, "is", factorial)
